40d80444923fa56c48bc4f77c5ef62202dc84e91
hzw <1254835285@qq.com>
2024-07-08 13:50:16 +0800

    five use

--- src/main/java/com/yaqi/common/Test.java
+++ src/main/java/com/yaqi/common/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello World!");
         System.out.println("Hello Idea!");
         System.out.println("Hello Git!");
+        System.out.println( "Hello Github!");
     }
 }
